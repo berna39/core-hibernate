1afac2fd7ba01ca54ce3d6110ddb0fe16393d2be
berna39 <kalemajoseph39@gmail.com>
2023-12-10 18:57:45 +0100

    :sparkles: merge entity to update

---
+++ src/main/java/com/terminator/domain/Book.java
@@ -0,0 +1,21 @@
+package com.terminator.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    
+    @Id
+    private String isbn; // natural key
+    private String title;
+}
